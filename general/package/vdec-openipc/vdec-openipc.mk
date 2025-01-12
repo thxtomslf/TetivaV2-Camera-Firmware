917e0dd712f0a4ddac022e8319012a2b63349ec2
Ivan Ivanov <purepvll@gmail.com>
2025-01-12 18:34:52 +0300

    Add configs for hisi ground station

--- general/package/vdec-openipc/vdec-openipc.mk
+++ general/package/vdec-openipc/vdec-openipc.mk
@@ -9,9 +9,6 @@ VDEC_OPENIPC_VERSION = HEAD
 
 VDEC_OPENIPC_LICENSE = MIT
 
-define VDEC_OPENIPC_BUILD_CMDS
-	$(MAKE) CC=$(TARGET_CC) DRV=$(HISILICON_OSDRV_HI3536DV100_PKGDIR)/files/lib -C $(@D)/vdec
-endef
 
 define VDEC_OPENIPC_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
@@ -21,7 +18,7 @@ define VDEC_OPENIPC_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(VDEC_OPENIPC_PKGDIR)/files/vdec.conf
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/vdec/vdec
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(VDEC_OPENIPC_PKGDIR)/files/vdec_crsf
 endef
 
 $(eval $(generic-package))
